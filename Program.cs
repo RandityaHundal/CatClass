4e68e1ea98cb56f4b07b5bdce615ac93eb2b7d1e
Randitya Hundal <67023582+RandityaHundal@users.noreply.github.com>
2023-11-26 14:16:03 -0800

    add code

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿// Cat class
+
+class Cat
+{
+    public string name;
+    public int age;
+
+    public void Meow()
+    {
+        Console.WriteLine(name + " says meow!");
+    }
+}
+
+// Main code
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Cat cat01 = new Cat();
+        cat01.name = "steven";
+        cat01.age = 1;
+        cat01.Meow();
+        
+        Cat cat02 = new Cat(); 
+        cat02.name = "lucy";
+        cat02.age = 2;
+        cat02.Meow();
+
+        Cat cat03 = new Cat();
+        cat03.name = "milo";
+        cat03.age = 3;
+        cat03.Meow();
+
+        Cat cat04 = new Cat();
+        cat04.name = "bella";
+        cat04.age = 4;
+        cat04.Meow();
+
+        Cat cat05 = new Cat();
+        cat05.name = "max";
+        cat05.age = 5;
+        cat05.Meow();
+
+        Cat cat06 = new Cat();
+        cat06.name = "lily";
+        cat06.age = 6;
+        cat06.Meow();
+
+    }
+}
